27a82a5a4d9009a9b254e15a18c4a85da7275c70
45848270 <shubhamchandra.yadav@students.mq.edu.au>
2021-10-03 19:43:19 +1100

    added characters and score script

--- Comp2160 week8 prac/Assets/Scripts/ScoreKeeper.cs
+++ Comp2160 week8 prac/Assets/Scripts/ScoreKeeper.cs
@@ -8,20 +8,29 @@ public class ScoreKeeper : MonoBehaviour
 
     public static ScoreKeeper instance;
     public int coinPoints = 10;
-    public Text scoreText;
+    public Text player1ScoreText;
+    public Text player2ScoreText;
 
-    public int score;
+    public int player1Score;
+    public int player2Score;
 
     public void Awake()
     {
         instance = this;
-        scoreText.text = "Score: 0" + score.ToString();
+        player1ScoreText.text = "Player1 Score: 0" + player1Score.ToString();
+        player2ScoreText.text = "Player2 Score: 0" + player2Score.ToString();
     }
 
-    public void AddScore()
+    public void AddPlayer1Score()
     {
-        Debug.Log("Adding Score");
-        score += coinPoints;
-        scoreText.text= "Score: " + score.ToString();
+        Debug.Log("Adding Score To Player1");
+        player1Score += coinPoints;
+        player1ScoreText.text= "Score: " + player1Score.ToString();
+    }
+    public void AddPlayer2Score()
+    {
+        Debug.Log("Adding Score To Player2");
+        player2Score += coinPoints;
+        player2ScoreText.text = "Score: " + player2Score.ToString();
     }
 }
