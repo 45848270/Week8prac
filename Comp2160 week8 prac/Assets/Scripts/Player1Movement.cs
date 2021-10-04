a97953cbf599d9175f0313b5f38614ad8da7940e
arjun-kalia <arjun.kalia@students.mq.edu.au>
2021-10-04 18:45:39 +1100

    player  2 added

--- Comp2160 week8 prac/Assets/Scripts/Player1Movement.cs
+++ Comp2160 week8 prac/Assets/Scripts/Player1Movement.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player2Movement : MonoBehaviour
+public class Player1Movement : MonoBehaviour
 {
         public float playerSpeed = 5f; // metre per second    
-
+        
 
     // Update is called once per frame
     void Update()
     {
-        float upDown = Input.GetAxis("Vertical1");
-        float leftRight = Input.GetAxis("Horizontal1");
+        float upDown = Input.GetAxis("Vertical2");
+        float leftRight = Input.GetAxis("Horizontal2");
 
         transform.Translate(upDown * playerSpeed * Vector3.forward * Time.deltaTime);       
         transform.Translate(leftRight * playerSpeed * Vector3.right * Time.deltaTime);
